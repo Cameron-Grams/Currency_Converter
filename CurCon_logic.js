0b31defd9857270d5f909eea2658a3c8f35bde21
Cameron <cameron.grams.2@gmail.com>
2017-08-31 22:43:52 -0400

    API restrictions explored in a new branch

--- CurCon_logic.js
+++ CurCon_logic.js
@@ -1,12 +1,6 @@
+const BASE_URL = 'http://apilayer.net/api/convert?access_key=';
+
 const API_KEY = 'fb3fadded8a585c425abb7d0da65f95a'; //for the currency converter API
-
-
-
-
-
-
-
-
 
 function resetDisplay( ){
   $( '.results' ).empty();
@@ -17,9 +11,19 @@ function sendConvert( event ){
   resetDisplay();    
   let fromCurrency = $( '#originalCurrency' ).val();
   let toCurrency = $( '#finalCurrency' ).val();
+  let convertAmount = $( '#currencyAmount' ).val();
+  let sendURL = BASE_URL + API_KEY + `&from=${ fromCurrency }&to=${ toCurrency }&amount=${ convertAmount}`;
+
+  $.getJSON( sendURL, displayConversion );
+  
   console.log( 'from currency is: ', fromCurrency );
   console.log( 'to currency is: ', toCurrency );
 }
+
+function displayConversion( data ){
+  console.log( data );
+}
+
 
 
 function main(){
