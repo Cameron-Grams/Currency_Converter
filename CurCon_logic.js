7044ce0c0013bfdac74e1e227335d5ccf1281d35
Cameron <cameron.grams.2@gmail.com>
2017-09-03 12:23:07 -0400

    continue to work on the UI

--- CurCon_logic.js
+++ CurCon_logic.js
@@ -8,24 +8,37 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtZXJvbjQzIiwiYSI6ImNqNzF1czNnajA2dmgzM24xN
 function resetDisplay( ){
   $( '.results' ).empty();
   $( '.flags' ).empty();
-  $( '.worldMap' ).empty();
+  $( '#worldMap' ).empty();
   $( '#currencyAmount' ).val( '' );
 }
 
 function sendConvert( event ){
   let toCurrency = $( '#finalCurrency' ).val();
   let sendURL = CONV_BASE_URL + CONV_API_KEY + `&currencies=${ toCurrency }&source=USD&format=1`;
-  $.getJSON( sendURL, createConversion );
+  let  results = $.getJSON( sendURL, createConversion );
+  if ( results.success === true ){
+    return results;
+  } else {
+    console.log( 'no curr inf' );
+    $( '.results' ).html( "No currency information available.")
+    return false;
+  }
 }
 
 function createConversion( data ){
+  console.log( 'data in converstion is ', data );
   let convertAmount = $( '#currencyAmount' ).val();
   let toCurrency = $( '#finalCurrency' ).val();
   let targetKey = 'USD' + toCurrency;
-  let targetRatio = data.quotes[ targetKey ];
-  let finalAmount = convertAmount * targetRatio;
-  displayConversion( convertAmount, finalAmount );
-  getFlag( toCurrency );
+  if ( data.quotes[ targetKey ] ){
+    let targetRatio = data.quotes[ targetKey ];
+    let finalAmount = (convertAmount * targetRatio).toFixed( 2 );
+    displayConversion( convertAmount, finalAmount );
+    getFlag( toCurrency );
+  } else {
+    $( '.results' ).html( "No currency information available.")
+  }
+  
 }
 
 function displayConversion( convertAmount, finalAmount ){
@@ -40,7 +53,6 @@ function getFlag( currency ){
 }
 
 function showFlag( data ){
-  console.log( data );
   let flag = data[ 0 ].flag;
   let location = data[ 0 ].latlng;
   let flagOnDOM = `<img src=${ flag }>`;
