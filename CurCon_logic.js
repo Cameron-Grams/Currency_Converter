69928690c6ffe87c75cda272e20c3fbd95e6f5a6
Cameron <cameron.grams.2@gmail.com>
2017-09-04 08:02:20 -0400

    fix url

--- CurCon_logic.js
+++ CurCon_logic.js
@@ -1,4 +1,4 @@
-const CONV_BASE_URL = 'http://apilayer.net/api/live?access_key=';
+const CONV_BASE_URL = 'https://apilayer.net/api/live?access_key=';
 const CONV_API_KEY = 'fb3fadded8a585c425abb7d0da65f95a'; //for the currency converter API
 
 const COUNTRY_URL = 'https://restcountries.eu/rest/v2/currency/';
