9f7a9a2aa1a7b65ec146bfd24556b75f96b1c5db
Cameron <cameron.grams.2@gmail.com>
2017-09-13 16:09:51 -0400

    work on the CORS issue

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ app.use( express.static( 'public' ) );
 
 
 //CORS commands
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
  
-
 app.listen( process.env.PORT || 1337, function(){
     console.log( 'app listening on 1337' )
 } );
